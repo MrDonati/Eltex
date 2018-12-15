5b87e17cbbca20fa3d4e9974b70f885bf7d55236
Artemchenko D.A <52art2010@mail.ru>
2018-12-15 15:08:44 +0700

    Spring

---
+++ Spring/src/main/java/spring/Html.java
@@ -0,0 +1,14 @@
+package spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Html {
+    @GetMapping("/")
+    public String ind()
+    {
+        String str = "xyi";
+        return "index";
+    }
+}
